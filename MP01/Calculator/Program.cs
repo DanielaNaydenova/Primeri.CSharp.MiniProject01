fd53850c88b618b070a82f0b390c9536e78b0f94
Daniela Naydenova <dani.naydenova@gmail.com>
2016-02-19 21:25:57 +0200

    Модул 06. Видео 03 - Основни принципи при създаване на архитектура на програма

---
+++ MP01/Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculator
+{
+	class MainClass
+	{
+
+		//Дефиниране на библиотеки
+		public UserInput.UInput uImput = new UserInput.UInput ();
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+			MainClass _program = new MainClass ();
+
+			_program.uImput.justTesting ();
+
+		}
+	}
+}
