2e019aaaba9116f42ab3ab2d02a5604606cbe2ac
Daniela Naydenova <dani.naydenova@gmail.com>
2016-02-20 21:01:24 +0200

    Модул 06. Видео 08 - Форматиране на първата формула

--- MP01/Calculations/Formula01.cs
+++ MP01/Calculations/Formula01.cs
@@ -4,6 +4,9 @@ namespace Calculations
 {
 	public class Formula01
 	{
+		//Библиотеки
+		private Colors.ForCLI _c = new Colors.ForCLI ();
+
 		public Formula01 () 	//Формула за обикновена строителна яма
 		{
 		}
@@ -17,6 +20,8 @@ namespace Calculations
 				if (param.Length > 1 && _userInput.Contains ("-п"))		//-п - за помощ
 				{
 					//Помощ за командата
+
+					help ();
 				}
 
 				if (param.Length > 1 && !_userInput.Contains ("-п"))		//-п - за помощ
@@ -26,10 +31,11 @@ namespace Calculations
 
 					if (runCalculations (param, out _result))
 					{
-						Console.Write ("Обемът на строителната яма е: ");
-						Console.WriteLine (_result.ToString ("N2"));
+						_c.Default (); 	Console.Write ("Обемът на строителната яма е: ");
+						_c.Result ();	Console.Write (_result.ToString ("N2"));
+						_c.Default ();	Console.WriteLine (" m3\n");
 					}else{
-						Console.WriteLine ("Има грешно въведен параметър. Можете да проверите синтаксиса с параметър '-п'");
+						_c.Default();	Console.WriteLine ("Има грешно въведен параметър. Можете да проверите синтаксиса с параметър '-п'\n");
 					}
 
 
@@ -62,8 +68,34 @@ namespace Calculations
 			_result = 0;
 			return false;
 		}
-		} 	 
-
+	 	 
 		//Помощ за командата
+		private void help ()
+		{		
+			_c.Result ();
+			Console.Write ("[яма]");
+			_c.Default ();
+			Console.WriteLine (" - команда за пресмятане на строителна яма");
+
+			_c.Command ();
+			Console.Write ("параметри: ");
+			_c.Default ();
+			Console.WriteLine ("a1 b1 a2 b2 h\n");
+
+			_c.Command ();
+			Console.Write ("a1 и b1");
+			_c.Default ();
+			Console.WriteLine (" - ширина и дължина на горната страна на изкопа");
+
+			_c.Command ();
+			Console.Write ("a2 и b2");
+			_c.Default ();
+			Console.WriteLine (" - ширина и дължина на долната страна на изкопа");
+
+			_c.Command ();
+			Console.Write ("h");
+			_c.Default ();
+			Console.WriteLine (" - височина на изкопа\n");
+		}
 	}
-	
+	}
