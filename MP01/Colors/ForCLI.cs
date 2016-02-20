431f39e535f6d19a5415f76cde3d5f365770dcf0
Daniela Naydenova <dani.naydenova@gmail.com>
2016-02-20 14:57:19 +0200

    Модул 06. Видео 06 - Форматиране на конзолно приложение

---
+++ MP01/Colors/ForCLI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Colors
+{
+	public class ForCLI
+	{
+		public ForCLI ()
+		{
+		}
+
+		public void Default ()
+		{
+			Console.ForegroundColor = ConsoleColor.Gray;
+		}
+
+		public void Prompt () //$
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGreen;
+		}
+
+		public void Command ()	//Командите от потребителя
+		{
+			Console.ForegroundColor = ConsoleColor.DarkYellow;
+		}
+
+		public void Result () 	//Резултат
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
+}
+
--- MP01/UserInput/UserInput.cs
+++ MP01/UserInput/UserInput.cs
@@ -5,16 +5,17 @@ namespace UserInput
 	public class UInput
 	{
 		About.Me about = new About.Me ();
+		Colors.ForCLI _c = new Colors.ForCLI ();
 
 		public UInput ()
 		{
-			Console.WriteLine ("Добре дошли в " + about.shortName + "\n" + about.version + "\n");
 
 		}
 
 		public void sayHello ()
 		{
-			
+			_c.Default ();
+			Console.WriteLine ("Добре дошли в " + about.shortName + "\n" + about.version + "\n");
 		}
 
 		public void getUserCommands ()
@@ -23,13 +24,30 @@ namespace UserInput
 			do
 			{
 				//Взимане на команда
-				Console.Write ("$ ");
-				_command = Console.ReadLine ();
+				_c.Prompt();	Console.Write ("$ ");
+				_c.Command ();	_command = Console.ReadLine ();
 
 				//Проверка за налични команди
-				if (_command.ToLower ().Contains ("команда1"))		Console.WriteLine ("Стартирана е Kоманда 1\n");
-				if (_command.ToLower ().Contains ("команда2"))		Console.WriteLine ("Стартирана е Kоманда 2\n");
-				if (_command.ToLower ().Contains ("команда3"))		Console.WriteLine ("Стартирана е Kоманда 3\n");
+				if (_command.ToLower ().Contains ("команда1"))
+				{
+					_c.Default ();	Console.Write ("Стартирана е ");
+					_c.Result ();	Console.WriteLine ("Kоманда 1\n");
+				}
+
+				if (_command.ToLower ().Contains ("команда2"))
+				{
+					_c.Default ();	Console.Write ("Стартирана е ");
+					_c.Result ();	Console.WriteLine ("Kоманда 2\n");
+				}
+
+				if (_command.ToLower ().Contains ("команда3"))
+				{
+					_c.Default ();	Console.Write ("Стартирана е ");
+					_c.Result ();	Console.WriteLine ("Kоманда 3\n");
+				}
+
+				/*if (_command.ToLower ().Contains ("команда2"))		Console.WriteLine ("Стартирана е Kоманда 2\n");
+				if (_command.ToLower ().Contains ("команда3"))		Console.WriteLine ("Стартирана е Kоманда 3\n");*/
 
 
 			}while (_command.ToLower () != "изход");
